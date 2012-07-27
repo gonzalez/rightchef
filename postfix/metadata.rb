3a67981faa37fa9ace7171c9aaedc04ecbc09909
Edwin <edds718@gmail.com>
2012-07-27 15:59:48 -0400

    testing smarthost option

--- postfix/metadata.rb
+++ postfix/metadata.rb
@@ -1,5 +1,17 @@
-maintainer       "me"
+maintainer       "github.com/gonzalez"
 license          "All rights reserved"
-description      "Installs/Configures postfix"
+description      "Postfix Utilities"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
+
+
+
+
+recipe "weblogic::sys_add_smarthost", "adds smarthost"
+
+
+attribute "postfix/smarthost",
+ :display_name => "Postfix Smarthost",
+ :description => "The Hostname or IP without or without port of where mail from this server should be relayed to formats: 1.2.3.4, 1.2.3.4:25, domain.com, domain.com:25",
+ :required => true,
+ :recipes => [ "postfix::add_postfix_smarthost"]
--- postfix/recipes/add_postfix_smarthost.rb
+++ postfix/recipes/add_postfix_smarthost.rb
@@ -1,1 +1,10 @@
 log "configured smarthost"
+
+SMARTHOST = node[:postfix][:smarthost]
+
+bash "add smarthost" do 
+  code <<-EOF
+  echo "relayhost=#{SMARTHOST}" >> "/etc/postfix/main.cf"
+  service postfix restart
+  EOF
+end
