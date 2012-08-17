b1acbc38ea4e235c540303144fe50c516d8ab716
Edwin <edds718@gmail.com>
2012-08-17 15:14:46 -0400

    new metadata.json

--- utils/metadata.rb
+++ utils/metadata.rb
@@ -1,6 +1,11 @@
-maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
+maintainer       "github.com/gonzalez"
 license          "All rights reserved"
-description      "Installs/Configures utils"
+description      "System Utilities"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
+
+
+recipe "utils::extract_file", "extracts file"
+
+attribute "utils/file",
+:display_name => "Filename"
