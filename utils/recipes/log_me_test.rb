a5d6053545f30e8b7c8889afaa87fad04de3634e
Richard Shade <rshade@rightscale.com>
2013-01-28 12:38:12 -0600

    second test with override

--- utils/recipes/log_me_test.rb
+++ utils/recipes/log_me_test.rb
@@ -1,3 +1,8 @@
 utils_log_me "Hello World" do
   action :write
 end
+
+utils_log_me "Hello World2" do
+  log_string "Hello World 3"
+  action :write
+end
--- utils/resources/log_me.rb
+++ utils/resources/log_me.rb
@@ -1,2 +1,2 @@
 actions :write
-attribute :log_string, :name_attribute
+attribute :log_string, :name_attribute => true
