4e317ca2a0a16e02fe0f2a7d5b817b0e850b2b80
Richard Shade <rshade@rightscale.com>
2013-01-29 16:42:00 -0600

    making mount directory a variable

--- utils/metadata.rb
+++ utils/metadata.rb
@@ -7,5 +7,15 @@ version          "0.0.1"
 
 recipe "utils::extract_test", "extracts file"
 recipe "utils::log_me_test", "writes to log"
-attribute "utils/extract/file", :display_name => "Filename"
 recipe "utils::create_tmpfs", "creates a tmpfs using a tmpfd mount and size"
+
+
+
+attribute "utils/extract/file", 
+  :display_name => "Filename"
+
+attribute "utils/tmpfs/mount",
+  :display_name => "TmpFS Mount Point",
+  :description => "TMPFS Mount Point",
+  :required => true,
+  :recipes => [ "utils::create_tmpfs" ]
--- utils/recipes/create_tmpfs.rb
+++ utils/recipes/create_tmpfs.rb
@@ -1,4 +1,4 @@
 utils_tmpfs "create tmpfs" do
-	tmpfs_mount '/tmp/edwin'
-    action :create
+  tmpfs_mount node[:utils][:tmpfs][:mount]
+  action :create
 end
