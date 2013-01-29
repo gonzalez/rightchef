a767d23ff79aa870814d230cdc06476391111b25
Edwin <edds718@gmail.com>
2013-01-29 16:40:51 -0500

    fix size on create tmpfs

--- utils/providers/tmpfs.rb
+++ utils/providers/tmpfs.rb
@@ -6,7 +6,7 @@ action :create do
 		mount -t tmpfs -o size=#{:tmpfs_size}M,mode=0744 tmpfs #{:tmpfs_mount}"
 
 		#make tmpfs available on every reboot
-		echo "tmpfs #{:tmpfs_mount} tmpfs size=500M,mode=0777 0 0" >> /etc/fstab
+		echo "tmpfs #{:tmpfs_mount} tmpfs size=#{:tmpfs_size}M,mode=0777 0 0" >> /etc/fstab
 		EOF
 	end
 
