fca1081a0f560c3fcedec9e94648149b962e4a56
Edwin <edds718@gmail.com>
2013-01-30 11:16:43 -0500

    changed tmpfs create to use interpreter

--- utils/resources/tmpfs.rb
+++ utils/resources/tmpfs.rb
@@ -1,4 +1,4 @@
 actions :create
 
-attribute :tmpfs_mount, [:required => true, :name_attribute => true, :default => '/tmp/right_tmpfs']
-attribute :tmpfs_size, [:default => '300', :name_attribute => true]
+attribute :tmpfs_mount, default =>'/tmp/right_tmpfs'
+attribute :tmpfs_size,  default => '300'
