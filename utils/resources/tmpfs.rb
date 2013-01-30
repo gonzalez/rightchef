d54655a65ec7bdb34359b775b13fcf49f02f9250
Edwin <edds718@gmail.com>
2013-01-30 17:31:17 -0500

    more colons

--- utils/resources/tmpfs.rb
+++ utils/resources/tmpfs.rb
@@ -1,4 +1,4 @@
 actions :create
 
-attribute :tmpfs_mount, default =>'/tmp/right_tmpfs'
-attribute :tmpfs_size,  default => '300'
+attribute :tmpfs_mount, :default => '/tmp/right_tmpfs'
+attribute :tmpfs_size,  :default => '300'
