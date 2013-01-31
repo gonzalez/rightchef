2a597800d134e17af033cb71850785fab9c372a4
Edwin <edds718@gmail.com>
2013-01-31 10:43:03 -0500

    pass hash to attribute

--- utils/resources/tmpfs.rb
+++ utils/resources/tmpfs.rb
@@ -1,4 +1,4 @@
 actions :create
 
-attribute :tmpfs_mount, :default => '/tmp/right_tmpfs'
-attribute :tmpfs_size,  :default => '300'
+attribute :tmpfs_mount, [:default => '/tmp/right_tmpfs', :name_attribute => true]
+attribute :tmpfs_size,  [:default => '300', :name_attribute => true]
