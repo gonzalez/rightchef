ea49cd04999dbb51ad994429341f95a455efa6e4
Edwin <edds718@gmail.com>
2013-01-31 13:31:46 -0500

    flag/flags

--- utils/providers/tmpfs.rb
+++ utils/providers/tmpfs.rb
@@ -2,7 +2,7 @@ action :create do
 	log "creating tmpsfs #{new_resource.tmpfs_mount}"
 	script "creating tmpsfs #{new_resource.tmpfs_mount}" do
 		interpreter "bash"
-		flag "-xe"
+		flags "-xe"
   		code <<-EOF
 		mkdir -p #{new_resource.tmpfs_mount}
 		mount -t tmpfs -o size=#{new_resource.tmpfs_size}M,mode=0744 tmpfs #{new_resource.tmpfs_mount}
