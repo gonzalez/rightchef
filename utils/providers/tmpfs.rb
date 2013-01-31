e97cece83fdd0dc447cf51c1189fb8df9179e9f9
Edwin <edds718@gmail.com>
2013-01-31 13:03:09 -0500

    new_resource added to var

--- utils/providers/tmpfs.rb
+++ utils/providers/tmpfs.rb
@@ -1,14 +1,14 @@
 action :create do
-	log "creating tmpsfs #{tmpfs_mount}"
-	script "creating tmpsfs #{tmpfs_mount}" do
+	log "creating tmpsfs #{new_resource.tmpfs_mount}"
+	script "creating tmpsfs #{new_resource.tmpfs_mount}" do
 		interpreter "bash"
 		flag "-xe"
   		code <<-EOF
-		mkdir -p #{tmpfs_mount}
-		mount -t tmpfs -o size=#{tmpfs_size}M,mode=0744 tmpfs #{tmpfs_mount}
+		mkdir -p #{new_resource.tmpfs_mount}
+		mount -t tmpfs -o size=#{new_resource.tmpfs_size}M,mode=0744 tmpfs #{new_resource.tmpfs_mount}
 
 		#make tmpfs available on every reboot
-		echo "tmpfs #{tmpfs_mount} tmpfs size=#{tmpfs_size}M,mode=0777 0 0" >> /etc/fstab
+		echo "tmpfs #{new_resource.tmpfs_mount} tmpfs size=#{new_resource.tmpfs_size}M,mode=0777 0 0" >> /etc/fstab
 		EOF
 	end
 
