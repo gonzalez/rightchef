de91b057fb8b1afbd1ef2d22822a360c05bb740d
Edwin <edds718@gmail.com>
2013-04-12 14:45:03 -0400

    test

--- utils/resources/tmpfs.rb
+++ utils/resources/tmpfs.rb
@@ -1,4 +1,4 @@
 actions :create
 
 attribute :tmpfs_mount, [:default => '/tmp/right_tmpfs', :name_attribute => true]
-attribute :tmpfs_size,  [:default => '300', :name_attribute => true]
+attribute :tmpfs_size,  [:default => '30', :name_attribute => true]
